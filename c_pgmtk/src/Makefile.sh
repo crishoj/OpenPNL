5c84e596b7f2cd5b7f10c978615477d6ed4aaaa1
trvs <trvs>
2004-10-13 17:06:12 +0000

    update

--- c_pgmtk/src/Makefile.sh
+++ c_pgmtk/src/Makefile.sh
@@ -16,9 +16,6 @@ ls *.c* | grep -v -e pnlMlLearningEngine\\.cpp -e pnlCreate.\*\\.cpp | sed -e 's
 
 echo '
 libpnl_la_LDFLAGS = -no-undefined -version-info $(LT_CURRENT):$(LT_REVISION):$(LT_AGE) -release $(LT_RELEASE) @LD_FLAGS@
-
-cvcart.cpp: cart/cvcart.cpp
-	cp cart/cvcart.cpp .
 '
 
 ) >Makefile.am
