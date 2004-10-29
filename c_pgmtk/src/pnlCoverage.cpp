bb76b7357fb6e061656ada75de72a727194d1a11
dilmah <dilmah>
2004-10-29 09:44:22 +0000

    trivia

--- c_pgmtk/src/pnlCoverage.cpp
+++ c_pgmtk/src/pnlCoverage.cpp
@@ -17,10 +17,8 @@
 #include "pnlConfig.hpp"
 #include "pnlCoverage.hpp"
 
-#if defined(WIN32)
+#if defined( WIN32 )
 #include <windows.h>
-#else
-#include <unistd.h>
 #endif
 
 #if defined( PNL_I_WANT_CODE_COVERAGE ) || defined( PNL_I_WANT_DECISION_COVERAGE )
@@ -46,7 +44,9 @@ bool PNL_API cc_if_gadget( bool cond, char const *file, int line )
 
 bool PNL_API cc_gen_gadget( char const *file, int line, ccStat stat )
 {
+#if defined( WIN32 )
     static int coun = 0;
+#endif
     static std::map< ccGenId, ccGenInfo > cc_map;
     ccGenInfo &info = cc_map[ccGenId( file, line, stat )];
     if ( info.stat == PNL_CC_UNREACHED )
@@ -55,15 +55,16 @@ bool PNL_API cc_gen_gadget( char const *file, int line, ccStat stat )
         std::cout << std::endl << "KODKOV: " << file << " " << line
                   << " -- reached, stat " << stat << std::endl;
     }
+
+// needed due to bad behaviour of W2K scheduler
+#if defined( WIN32 )
     if ( ++coun > 10000 )
     {
         coun = 0;
-#if 1
         Sleep( 1 );
-#else
-        sleep( 1 );
-#endif
     } 
+#endif
+
     return false;
 }
 
