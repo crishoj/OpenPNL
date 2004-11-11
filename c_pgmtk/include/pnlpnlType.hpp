8b96ab699d0546caf8a138df2ccb2e047828ae06
seninandrew <seninandrew>
2004-11-11 18:03:07 +0000

    Add file(s) for PNL RTTI support

---
+++ c_pgmtk/include/pnlpnlType.hpp
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+//                INTEL CORPORATION PROPRIETARY INFORMATION                //
+//   This software is supplied under the terms of a license agreement or   //
+//  nondisclosure agreement with Intel Corporation and may not be copied   //
+//   or disclosed except in accordance with the terms of that agreement.   //
+//       Copyright (c) 2003 Intel Corporation. All Rights Reserved.        //
+//                                                                         //
+//  File:      pnlpnlType.hpp                                              //
+//                                                                         //
+//  Purpose:   Class definition for main class for RTTI in PNL             //
+//                                                                         //
+//                                                                         //
+//  Author(s): (in alphabetical order)                                     //
+//             Abrosimova, Bader, Chernishova, Gergel, Labutina, Senin,    //
+//             Sidorov, Sysoyev, Vinogradov                                //
+//                                                                         //
+//             NNSU                                                        //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __PNLPNLTYPE_HPP__
+#define __PNLPNLTYPE_HPP__
+
+#include <string>
+
+//This is the main class for RTTI in PNL
+//It does not support multiple inheritance
+class PNL_API CPNLType
+{
+public:
+  CPNLType(const std::string &TypeName, const CPNLType *const Parent = NULL):
+     m_TypeName(TypeName),
+     m_pParent(Parent)
+  {}
+
+  bool operator==(const CPNLType &OtherType) const
+  {
+    return (&OtherType == this);
+  }
+
+  bool IsDerived(const CPNLType &ParentType) const
+  {
+    if (ParentType == *this) 
+    {
+      return true;
+    }
+    else 
+    {
+      if (this->m_pParent != NULL)
+      {
+        return this->m_pParent->IsDerived(ParentType);
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    return false;
+  }
+
+  const std::string &GetTypeName() const
+  {
+    return m_TypeName;
+  }
+  
+protected:
+private:
+  const std::string m_TypeName;
+  const CPNLType *const m_pParent;
+};
+
+#endif
---
+++ c_pgmtk/src/pnlpnlType.cpp
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+//                INTEL CORPORATION PROPRIETARY INFORMATION                //
+//   This software is supplied under the terms of a license agreement or   //
+//  nondisclosure agreement with Intel Corporation and may not be copied   //
+//   or disclosed except in accordance with the terms of that agreement.   //
+//       Copyright (c) 2003 Intel Corporation. All Rights Reserved.        //
+//                                                                         //
+//  File:      pnlpnlType.cpp                                              //
+//                                                                         //
+//  Purpose:   Implementation of main class for RTTI in PNL                //
+//             and static variables of all classes                         //
+//             (static variables were moved to corresponding cpp - files)  //
+//                                                                         //
+//  Author(s): (in alphabetical order)                                     //
+//             Abrosimova, Bader, Chernishova, Gergel, Labutina, Senin,    //
+//             Sidorov, Sysoyev, Vinogradov                                //
+//                                                                         //
+//             NNSU                                                        //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+
+#include "pnlConfig.hpp"
+#include "pnlObject.hpp"
+
+PNL_USING
