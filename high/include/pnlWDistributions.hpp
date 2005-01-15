a444947aae17565fc88a4c8bd8e146c33fee4f44
abon <abon>
2005-01-15 11:38:42 +0000

    added new parameter in function  void CreateDefaultDistribution();

--- high/include/pnlWDistributions.hpp
+++ high/include/pnlWDistributions.hpp
@@ -28,13 +28,14 @@ public:
     bool IsValid(int iNode);
     void GetNodeTypeInfo(bool *pbDiscrete, int *pSize, int iNode);
     void ResetDistribution(int iNode, pnl::CFactor &ft);
-    void FillData(TokArr &value, TokArr &probability, TokArr &parentValue);
+    void FillData(TokArr &value, TokArr &probability, TokArr &parentValue, pnl::EMatrixType matType = pnl::matTable);
 
 protected:
     virtual void DoNotify(int message, int iNode, ModelEngine *pObj);
     virtual int InterestedIn() const { return eDelNode|eChangeNState|eChangeParentNState|eInit; }
     TokenCover &Token() { return *m_pToken; }
     WGraph &Graph();
+    bool IsDiscrete(int iNode) const;
 
 private:
     TokenCover *m_pToken;			// token handler
