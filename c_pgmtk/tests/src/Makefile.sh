0a757b5290f2e03c6beebc9b03071a7c18542e2d
vinogradovroman <vinogradovroman>
2005-01-27 15:13:42 +0000

    Added target to check package (when we run make check)

--- c_pgmtk/tests/src/Makefile.sh
+++ c_pgmtk/tests/src/Makefile.sh
@@ -1,17 +1,24 @@
 CC=gcc
 (
 echo "
-bin_PROGRAMS=tests_pnl_c
+check_PROGRAMS = tests_pnl_c
 
 include \$(top_srcdir)/Makefile.incl
 
-INCLUDES=\$(PNL_INCL) -I../include -I\$(top_srcdir)/trs/include
-tests_pnl_c_SOURCES=\\"
+INCLUDES = \$(PNL_INCL) -I\$(srcdir)/../include -I\$(top_srcdir)/trs/include
+
+tests_pnl_c_SOURCES =     \\"
 ls *.c* \
 | grep -v -e MATLAB -e TJTreeInference\\.cpp -e DSLPNLConverter \
 | sed -e 's/^/     /' -e 's/$/     \\/' \
 | sed -e '$ s/     \\$//'
 echo '
-tests_pnl_c_LDADD=$(PNL_LIBS) $(top_srcdir)/trs/src/libtrs.a
+tests_pnl_c_LDADD = $(PNL_LIBS) $(top_builddir)/trs/src/libtrs.a
+
+TESTS = run_pnlcheck
+
+run_pnlcheck: tests_pnl_c
+	@echo "./tests_pnl_c -R FAIL -s $(top_builddir)/checkfails.sum -m -B" >$@; \
+	@chmod a+x $@
 '
 ) >Makefile.am
