2d8be22a6294469b6380ead9b9efa4cc680cd759
seninandrew <seninandrew>
2005-03-18 15:43:09 +0000

    Fix some bugs with different types of numeration in the function TokArr BayesNet::GetPTabular(TokArr child, TokArr parents).

--- high/source/BNet.cpp
+++ high/source/BNet.cpp
@@ -274,8 +274,11 @@ TokArr BayesNet::GetPTabular(TokArr child, TokArr parents)
     }
 
     Vector<int> childNd, childVl;
+    Vector<int> childNdInner;
     int i;
     Net().ExtractTokArr(child, &childNd, &childVl);
+
+    Net().Graph()->IGraph( &childNd, &childNdInner);
 
     // node for all child must be same (its may differ by combination only)
     for(i = childNd.size(); --i > 0; ++i)
@@ -292,10 +295,12 @@ TokArr BayesNet::GetPTabular(TokArr child, TokArr parents)
     }
 
     Vector<int> parentNds, parentVls;
+    Vector<int> parentNdsInner;
     int nparents = parents.size();
     if( nparents )
     {
 	Net().ExtractTokArr(parents, &parentNds, &parentVls);
+	Net().Graph()->IGraph( &parentNds, &parentNdsInner);
 	if( parentVls.size() == 0 ||
 	    std::find(parentVls.begin(), parentVls.end(), -1 ) != parentVls.end() )
 	{
@@ -304,21 +309,24 @@ TokArr BayesNet::GetPTabular(TokArr child, TokArr parents)
     }
     else
     {
-	Net().Graph()->Graph()->GetParents( childNd.front(), &parentNds );
+	Net().Graph()->Graph()->GetParents( childNdInner.front(), &parentNdsInner );
+	Net().Graph()->IOuter( &parentNdsInner, &parentNds);
 	nparents = parentNds.size();
 	parentVls.assign(nparents, -1);
     }
 
     parentNds.resize(nparents + 1);
+    parentNdsInner.resize(nparents + 1);
     parentVls.resize(nparents + 1);
 
-    const pnl::CFactor * cpd = Model()->GetFactor(childNd.front());
+    const pnl::CFactor * cpd = Model()->GetFactor(childNdInner.front());
     const pnl::CMatrix<float> *mat = cpd->GetMatrix(pnl::matTable);
 	
     TokArr result = "";
     for( i = 0; i < nchldComb; i++ )
     {
 	parentNds[nparents] = childNd.front();
+	parentNdsInner[nparents] = childNdInner.front();
 	parentVls[nparents] = childVl[i];
 	result << Net().CutReq( parentNds, parentVls, mat);
     }
