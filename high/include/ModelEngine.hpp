a6a7266535f338f699254ea55f06dd8b7e29427e
trvs <trvs>
2005-04-25 01:40:46 +0000

    fix ModelEngine::StopSpyTo

--- high/include/ModelEngine.hpp
+++ high/include/ModelEngine.hpp
@@ -68,28 +68,7 @@ public:
 	pSpy->m_apSpy.push_back(this);
 	m_apSpyTo.push_back(pSpy);
     }
-    void StopSpyTo(ModelEngine *pSpy)
-    {
-	int i;
-	for(i = pSpy->m_apSpy.size(); --i >= 0; )
-	{
-	    if(pSpy->m_apSpy[i] == this)
-	    {
-		pSpy->m_apSpy[i] = pSpy->m_apSpy.back();
-		pSpy->m_apSpy.pop_back();
-		break;
-	    }
-	}
-	for(i = m_apSpyTo.size(); --i >= 0; )
-	{
-	    if(m_apSpyTo[i] == this)
-	    {
-		m_apSpyTo[i] = m_apSpyTo.back();
-		m_apSpyTo.pop_back();
-		break;
-	    }
-	}
-    }
+    void StopSpyTo(ModelEngine *pSpy);
 
 protected:
     virtual void DoNotify(const Message &msg) = 0;
--- high/source/ModelEngine.cpp
+++ high/source/ModelEngine.cpp
@@ -17,4 +17,36 @@ ModelEngine::~ModelEngine()
     }
 }
 
+void ModelEngine::StopSpyTo(ModelEngine *pSpy)
+{
+    static const char fname[] = "ModelEngine::StopSpyTo";
+    int i;
+    for(i = pSpy->m_apSpy.size(); --i >= 0; )
+    {
+	if(pSpy->m_apSpy[i] == this)
+	{
+	    pSpy->m_apSpy[i] = pSpy->m_apSpy.back();
+	    pSpy->m_apSpy.pop_back();
+	    break;
+	}
+    }
+    if(i < 0)
+    {
+	ThrowInternalError("Object not found", fname);
+    }
+    for(i = m_apSpyTo.size(); --i >= 0; )
+    {
+	if(m_apSpyTo[i] == pSpy)
+	{
+	    m_apSpyTo[i] = m_apSpyTo.back();
+	    m_apSpyTo.pop_back();
+	    break;
+	}
+    }
+    if(i < 0)
+    {
+	ThrowInternalError("Object not found", fname);
+    }
+}
+
 PNLW_END
