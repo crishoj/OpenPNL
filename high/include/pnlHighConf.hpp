04f66c8a80d27bffc2d71706027ba568d49e6417
seninandrew <seninandrew>
2005-05-10 12:13:48 +0000

    A small bug in ALLOW_TESTING mocro was fixed

--- high/include/pnlHighConf.hpp
+++ high/include/pnlHighConf.hpp
@@ -74,7 +74,7 @@ extern PNLHIGH_API TokArr decision;
 extern PNLHIGH_API TokArr value;
 
 class TestsPnlHigh;
-#define ALLOW_TESTING friend TestsPnlHigh;
+#define ALLOW_TESTING friend class TestsPnlHigh;
 
 PNLW_END
 
