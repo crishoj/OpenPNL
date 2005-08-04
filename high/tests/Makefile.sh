f3c2dbb4e3b660c95bdf164160a66c45573b159a
sidorovsergei <sidorovsergei>
2005-08-04 09:55:16 +0000

    updated tests project

--- high/tests/Makefile.sh
+++ high/tests/Makefile.sh
@@ -4,12 +4,12 @@ check_PROGRAMS = test_high
 
 include \$(top_srcdir)/Makefile.incl
 
-INCLUDES = \$(PNL_INCL) -I\$(srcdir)/../include -I\$(srcdir)/../examples -I\$(srcdir)
+INCLUDES = \$(PNL_INCL) -I\$(srcdir)/../include -I\$(srcdir)/../examples -I\$(srcdir) -I\$(srcdir)/../../trs/include
 
 test_high_SOURCES =     \\"
 ls *.cpp | sed -e 's/^/     /' -e 's/$/     \\/' -e '$ s/     \\$//'
 echo "
-test_high_LDADD = \$(PNL_LIBS) \$(top_builddir)/high/source/libhigh.la
+test_high_LDADD = \$(PNL_LIBS) \$(top_builddir)/high/source/libhigh.la \$(top_builddir)/trs/src/libtrs.a
 "
 ) >Makefile.am
 
